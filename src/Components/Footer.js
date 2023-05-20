b524e1a8aa66cd4da04c9e6cfad7866899dc2762
murugappansrija <srijamurugappan66@gmail.com>
2023-05-21 00:44:27 +0530

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+
+    return(
+        <>
+            <footer className="py-3 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">
+                        Thankyou for visiting our site
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
